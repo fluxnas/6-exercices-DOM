968e750644c1c379af9ee040a3be6b375bb01c36
Hanna <104756701+fluxnas@users.noreply.github.com>
2023-02-18 04:04:34 +0100

    Delete 02b-selectors directory

--- 02b-selectors/script.js
+++
@@ -1,6 +0,0 @@
-var importante = document.querySelectorAll(".important");
-for (i=0; i<importante.lenght; i++) {
-	importante = element.title;
-	i++;
-}
-console.log(document.querySelectorAll(".important"))
